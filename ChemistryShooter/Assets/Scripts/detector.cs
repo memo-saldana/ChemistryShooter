3c10b71cd3f3a2436834d49dc12b4d4eac2a7723
Jose Guillermo Saldana <memo.sal98@gmail.com>
2019-04-06 18:09:00 -0600

    Created project, and starting scenes

---
+++ ChemistryShooter/Assets/Scripts/detector.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class detector : MonoBehaviour
+{
+    // Start is called before the first frame update
+
+    RaycastHit hit;
+    Ray rayo;
+
+    // Draw the Ray in RED
+    // Update is called once per frame
+    void Update()
+    {
+        Debug.DrawRay(rayo.origin, rayo.direction * 10, Color.red );
+        rayo =  Camera.main.ScreenPointToRay(Input.mousePosition);
+        if (Physics.Raycast(rayo,out hit)){
+          print("I see a: "+ hit.transform.name);
+        } else {
+          print("I see nothing, nothing :c");
+        }
+    }
+}
