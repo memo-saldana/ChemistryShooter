d65f14413f502d0cde13a9a6391ef53bf9d39288
Jose Guillermo Saldana <memo.sal98@gmail.com>
2019-04-06 23:12:28 -0600

    First element prefab done, spawner in process

---
+++ ChemistryShooter/Assets/Scripts/Spawner.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Spawner : MonoBehaviour
+{ 
+  public GameObject[] enemies;
+  [SerializeField] private Vector3 spawnValues;
+  public float spawnWait;
+  public float spawnMostWait;
+  public float spawnLeastWait;
+  public int startWait;
+
+  public int enemiesAmount;
+
+  int randEnemy;
+    // Start is called before the first frame update
+
+    void Start()
+    {
+      StartCoroutine( waitSpawner());
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        spawnWait = Random.Range( spawnLeastWait, spawnMostWait );
+    }
+
+    IEnumerator waitSpawner()
+    {
+      yield return new WaitForSeconds(startWait);
+
+      while( enemiesAmount>0 )
+      {
+        randEnemy = Random.Range(0, enemies.Length )
+
+        Vector3 spawnPosition = new Vector3( Random.Range( -spawnValues.x, spawnValues.x) , 1, Random.Range( -spawnValues.z, spawnValues.z) );
+
+        Instantiate(enemies[randEnemy], spawnPosition + transform.TransformPoint( 0, 0, 0 ), gameObject.transform.rotation );
+
+        yield return new WaitForSeconds(spawnWait);
+        enemiesAmount--;
+      }
+    }
+}
