7c140fec25dd4811db3ef91ce641f51a8a64d0a7
Jose Guillermo Saldana <memo.sal98@gmail.com>
2019-04-06 20:04:51 -0600

    Pause menu 1st iteration, projectile works

---
+++ ChemistryShooter/Assets/Scripts/PauseManager.cs
@@ -0,0 +1,41 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+#if UNITY_EDITOR
+using UnityEditor;
+#endif
+
+// Code from unity3d.com tutorials
+public class PauseManager : MonoBehaviour {
+    
+    Canvas canvas;
+    
+    void Start()
+    {
+        canvas = GetComponent<Canvas>();
+        canvas.enabled = false;
+    }
+    
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            Pause();
+        }
+    }
+    
+    public void Pause()
+    {
+        canvas.enabled = !canvas.enabled;
+        Time.timeScale = Time.timeScale == 0 ? 1 : 0;
+    }
+    
+    public void Quit()
+    {
+        #if UNITY_EDITOR 
+        EditorApplication.isPlaying = false;
+        #else 
+        Application.Quit();
+        #endif
+    }
+}
---
+++ ChemistryShooter/Assets/Scripts/shoot.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class shoot : MonoBehaviour
+{
+   public Rigidbody bala;
+   public float speed;
+   Rigidbody clone;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetButton("Fire1")){
+            clone=Instantiate(bala,transform.position,transform.rotation);
+            clone.velocity=transform.TransformDirection(0,0,-speed);
+            Destroy(clone.gameObject,2);
+        }
+    }
+}
