d29b3cd5398a38f977f63b9699f0c921ff4f42f6
monicaanavap <31547577+monicaanavap@users.noreply.github.com>
2019-04-09 00:15:49 -0500

    Interfaces

---
+++ ChemistryShooter/Assets/Scripts/SceneSwitcher.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement; 
+
+public class SceneSwitcher : MonoBehaviour
+{
+    public void GotoStartScene()
+    {
+        SceneManager.LoadScene("Start");
+    }
+
+    public void GotoCreditsScene()
+    {
+        SceneManager.LoadScene("Credits");
+    }
+
+    public void GotoMainGameScene()
+    {
+        SceneManager.LoadScene("MainGame");
+    }
+
+    public void GotoInstructionsScene()
+    {
+        SceneManager.LoadScene("Instructions");
+    }
+
+    public void GotoWinScene()
+    {
+        SceneManager.LoadScene("Win");
+    }
+}
